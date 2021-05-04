fd7260f4bf91ff6cfaf4566f664a9f04cd46fbec
Megan Wilde <miss.megan@gmail.com>
2021-05-04 10:55:42 -0500

    all specifications now working; just need to fix styling

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -150,35 +150,6 @@ function load_mailbox(mailbox) {
       email_contents_row.style.display = 'none';
       email_contents_row.className = 'row';
       email_box.append(email_contents_row);
-
-      let email_contents_col = document.createElement('div');
-      email_contents_col.className = 'col-12';
-      email_contents_row.append(email_contents_col);
-
-      let email_header_row1 = document.createElement('div');
-      email_header_row1.className = 'row';
-      let email_header_row2 = document.createElement('div');
-      email_header_row2.className = 'row';
-      let email_body = document.createElement('div');
-      email_body.className = 'row';
-
-      email_header_row1.innerHTML = `<div class="col-1" id="close-icon"><i class="far fa-window-close"></i></div>
-      <div class="col-4" id="sender${element.id}">From: ${element.sender}</div>
-      <div class="col-4" id="subject${element.id}">Subject: ${element.subject}</div>
-      <div class="col-3" id="sent${element.id}">Sent on ${element.timestamp}</div>`;
-      email_header_row2.innerHTML = `<div class="col-1"></div>
-      <div class="col-8" id="recipients${element.id}">To: ${element.recipients}</div>
-      <div class="col-3" id="email-buttons"></div>`;
-      
-      const body_text = element.body;
-      let body_col = document.createElement('div');
-      body_col.className = 'col-12 email-body';
-      body_col.innerText = body_text;
-      email_body.append(body_col);
-
-      email_contents_col.append(email_header_row1);
-      email_contents_col.append(email_header_row2);
-      email_contents_col.append(email_body);
       
     });
   });
@@ -198,78 +169,191 @@ function view_email(id) {
     localStorage.setItem('viewing', id);
   }
 
-  // show email contents and hide mailbox row for selected email
+  // select requested email_contents_row
   let email_contents_row = document.querySelector(`#email-contents-row${id}`);
+
+  // inside row, create divs for requested email col and nested header rows and body text row
+  let email_contents_col = document.createElement('div');
+  email_contents_col.className = 'col-12';
+  email_contents_row.append(email_contents_col);
+  let email_header_row1 = document.createElement('div');
+  email_header_row1.className = 'row';
+  let email_header_row2 = document.createElement('div');
+  email_header_row2.className = 'row';
+  let email_body = document.createElement('div');
+  email_body.className = 'row';
+
+  // get requested email contents using get request
+  fetch(`/emails/${id}`)
+  .then(response => response.json())
+  .then(email => {
+
+    let close_icon = document.createElement('div');
+    close_icon.className = 'col-1';
+    close_icon.innerHTML = '<i class="far fa-window-close"></i>';
+
+    // add event listener on close icon to close email
+    close_icon.addEventListener('click', function () {
+      close_email(id);
+    });
+    email_header_row1.append(close_icon);
+
+    // add remaining header guts for requested email
+    let sender_div = document.createElement('div');
+    sender_div.className = 'col-4';
+    sender_div.id = `sender${email.id}`;
+    sender_div.innerHTML = `From: ${email.sender}`;
+    email_header_row1.append(sender_div);
+
+    let subject_div = document.createElement('div');
+    subject_div.className = 'col-4';
+    subject_div.id = `subject${email.id}`;
+    subject_div.innerHTML = `Subject: ${email.subject}`;
+    email_header_row1.append(subject_div);
+
+    let timestamp_div = document.createElement('div');
+    timestamp_div.className = 'col-3';
+    timestamp_div.id = `sent${email.id}`;
+    timestamp_div.innerHTML = `Sent on ${email.timestamp}`;
+    email_header_row1.append(timestamp_div);
+
+    // add spacer and recipients divs to header row 2 
+    let spacer_div = document.createElement('div');
+    spacer_div.className = 'col-1';
+    email_header_row2.append(spacer_div);
+
+    let recipients_div = document.createElement('div');
+    recipients_div.className = 'col-8';
+    recipients_div.id = `recipients${email.id}`;
+    recipients_div.innerHTML = `To: ${email.recipients}`;
+    email_header_row2.append(recipients_div);
+
+    // add div for reply/archive buttons into header row 2
+    let email_buttons = document.createElement('div');
+    email_buttons.className = 'col-3';
+    email_buttons.id = 'email-buttons';
+    email_header_row2.append(email_buttons);
+    
+    // show buttons for reply and archive, if there aren't already buttons
+    if (!email_buttons.querySelector('button')) {
+
+      // create reply button and add to buttons div
+      let reply_btn = document.createElement('button');
+      reply_btn.type = 'button';
+      reply_btn.className = 'btn btn-light btn-sm';
+      reply_btn.innerHTML = '<i class="fas fa-reply"></i> Reply';
+      email_buttons.append(reply_btn);
+
+      // set event listener to run reply function when reply button clicked
+      reply_btn.addEventListener('click', function () {
+        reply(id);
+      });
+
+      // create archive/unarchive button and add to buttons div
+      let archive_btn = document.createElement('button');
+      archive_btn.type = 'button';
+      archive_btn.className = 'btn btn-light btn-sm';
+
+      // check if viewing archive mailbox
+      let mailbox_name = document.querySelector('#emails-view > h3').innerHTML;
+
+      // if archive, name of button is unarchive; else archive
+      if (mailbox_name === 'Archive') {
+        archive_btn.innerHTML = '<i class="fas fa-inbox"></i> Unarchive';
+      } else {
+        archive_btn.innerHTML = '<i class="fas fa-archive"></i> Archive';
+      }
+
+      // add archive button to buttons div in open email
+      email_buttons.append(archive_btn);
+
+      // run archive function with archive/unarchive button clicked
+      archive_btn.addEventListener('click', async function () {
+
+        // get message info for archive success display
+        let sender = email_contents_row.querySelector(`#sender${id}`).innerHTML.slice(5);
+        let subject = email_contents_row.querySelector(`#subject${id}`).innerHTML.slice(8);
+
+        if (mailbox_name === 'Archive') {
+          // if archive mailbox open and button clicked, set archived to false
+          // use async function and await fetch to send put request with archive status
+          // by doing so, function won't proceed until promise received
+          let response = await fetch(`/emails/${id}`, {
+            method: 'PUT',
+            body: JSON.stringify({
+                archived: false
+            })
+          })
+          // after promise, load inbox so it will show unarchived message
+          if (response) {
+            load_mailbox('inbox');
+            display_message('info', `Unarchived message from ${sender} re: ${subject}`);
+          }
+
+        } else {
+
+          // if archive button is clicked, use fetch request to set archived to true
+          let response = await fetch(`/emails/${id}`, {
+            method: 'PUT',
+            body: JSON.stringify({
+                archived: true
+            })
+          })
+          // after promise, then load mailbox so archived message won't still show up
+          if (response) {
+            load_mailbox('inbox');
+            display_message('info', `Archived message from ${sender} re: ${subject}`);
+          }
+
+        }
+        
+      });
+    }
+
+    // add body text into div as innerText to retain message formatting
+    const body_text = email.body;
+    let body_col = document.createElement('div');
+    body_col.className = 'col-12 email-body';
+    body_col.innerText = body_text;
+    email_body.append(body_col);
+    email_contents_col.append(email_header_row1);
+    email_contents_col.append(email_header_row2);
+    email_contents_col.append(email_body);
+  
+  });
+
+  // show email contents and hide mailbox row for selected email
   email_contents_row.style.display = "flex";
   email_contents_row.previousSibling.style.display = "none";
 
-  // mark email as read with PUT request
-  fetch(`/emails/${id}`, {
-    method: 'PUT',
-    body: JSON.stringify({
-        read: true
-    })
-  })
-
-  // add event listener on close icon to close email
-  let close_icon = email_contents_row.querySelector('#close-icon');
-  close_icon.addEventListener('click', function () {
-    close_email(id);
-  });
-
-  // show buttons for reply and archive, if there aren't already buttons
-  let email_buttons = email_contents_row.querySelector('#email-buttons');
-  if (!email_buttons.querySelector('button')) {
-
-    let reply_btn = document.createElement('button');
-    let archive_btn = document.createElement('button');
-    reply_btn.type = 'button';
-    reply_btn.className = 'btn btn-light btn-sm';
-    reply_btn.innerHTML = '<i class="fas fa-reply"></i> reply';
-    archive_btn.type = 'button';
-    archive_btn.className = 'btn btn-light btn-sm';
-    archive_btn.innerHTML = '<i class="fas fa-archive"></i> archive';
-    email_buttons.append(reply_btn);
-    email_buttons.append(archive_btn);
-
-    reply_btn.addEventListener('click', function () {
-      reply(id);
-    });
-
-    archive_btn.addEventListener('click', async function () {
-
-      // use async function and await fetch to send put request with archive status
-      // by doing so, function won't proceed until promise received
-      const response = await fetch(`/emails/${id}`, {
-        method: 'PUT',
-        body: JSON.stringify({
-            archived: true
-        })
+  async function mark_as_read() { 
+    // mark email as read with PUT request
+    // use await/async so request will finish and read status will take effect before displaying message
+    let read_response = await fetch(`/emails/${id}`, {
+      method: 'PUT',
+      body: JSON.stringify({
+          read: true
       })
-
-      // get message info for success display
-      let sender = email_contents_row.querySelector(`#sender${id}`).innerHTML.slice(5);
-      let subject = email_contents_row.querySelector(`#subject${id}`).innerHTML.slice(8);
-      // after promise, then load mailbox so archived message won't still show up
-      if (response) {
-        load_mailbox('inbox');
-        display_message('info', `Archived message from ${sender} re: ${subject}`);
-      }
-      
-
-    });
+    })
   }
+  mark_as_read();
 
 }
 
+// close email - called by click on close icon in open email
 function close_email(id) {
+  // get email contents row using email id
   let closing_email_contents = document.querySelector(`#email-contents-row${id}`);
+  // hide email contents row
   closing_email_contents.style.display = "none";
+  // use previousSibling to show mailbox row
   let show_mailbox_row = closing_email_contents.previousSibling;
   show_mailbox_row.style.display = "flex";
   show_mailbox_row.parentElement.className = 'col-12 email-box read';
   let last_icon = closing_email_contents.previousSibling.querySelector('#status-icon');
   last_icon.innerHTML = '<i class="far fa-envelope-open"></i>';
+
+  // clear local storage of viewing: email id
   localStorage.clear();
 }
 
@@ -280,6 +364,7 @@ function reply(id) {
   document.querySelector('#compose-view').style.display = 'block';
   document.querySelector('#messages').innerHTML = '';
 
+  // use fetch to get email contents
   fetch(`/emails/${id}`)
   .then(response => response.json())
   .then(email => {
@@ -291,24 +376,18 @@ function reply(id) {
     if (subject_start === 'Re:') {
       document.querySelector('#compose-subject').value = email.subject;
     } else {
-      console.log('subject does not contain re! in substring', subject_start);
+      document.querySelector('#compose-subject').value = `Re: ${email.subject}`;
     }
     
-
+    // include text from original email in body with formatting
     const response_body = `
     
     ----- 
     On ${email.timestamp}, ${email.sender} wrote:
 
     ${email.body}`;
-
     document.querySelector('#compose-body').value = response_body;
 
   });
 
 }
-
-///// TO DO TOMORROW!
-// create unarchive button on archive box
-// add get request for individual email in view email, and move filling in email contents to view email function
-// fix shitty styling issues with flexbox rows/email body
